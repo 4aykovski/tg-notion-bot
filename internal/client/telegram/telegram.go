eb2fcf61cac584c10edc51965a66d9690c192067
4aykovski <kan89kan@gmail.com>
2024-02-01 17:51:50 +0300

    feat: integration telegram client

--- internal/client/telegram/telegram.go
+++ internal/client/telegram/telegram.go
@@ -2,18 +2,22 @@ package telegram
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"path"
 	"strconv"
 
+	"github.com/4aykovski/tg-notion-bot/config"
 	"github.com/4aykovski/tg-notion-bot/lib/helpers"
+	"github.com/cavaliergopher/grab/v3"
 )
 
 const (
-	getUpdatesMethod  = "getUpdates"
-	sendMessageMethod = "sendMessage"
+	getUpdatesMethod  = "/getUpdates"
+	sendMessageMethod = "/sendMessage"
+	getFileMethod     = "/getFile"
 )
 
 type Client struct {
@@ -22,12 +26,15 @@ type Client struct {
 	client   http.Client
 }
 
-func New(host string, token string) *Client {
+func New(host string, token string) (*Client, error) {
+	if token == "" {
+		return nil, helpers.ErrWrapIfNotNil("can't create telegram client", fmt.Errorf("token wasn't specified"))
+	}
 	return &Client{
 		host:     host,
 		basePath: newBasePath(token),
 		client:   http.Client{},
-	}
+	}, nil
 }
 
 func newBasePath(token string) string {
@@ -68,13 +75,48 @@ func (c *Client) SendMessage(chatId int, text string) error {
 	return nil
 }
 
+func (c *Client) FileInfo(fileId string) (*File, error) {
+	q := url.Values{}
+	q.Add("file_id", fileId)
+
+	jsonRes, err := c.doRequest(getFileMethod, q)
+	if err != nil {
+		return nil, helpers.ErrWrapIfNotNil("can't get file", err)
+	}
+
+	res, err := c.fileResponse(jsonRes)
+	if err != nil {
+		return nil, helpers.ErrWrapIfNotNil("can't get file", err)
+	}
+
+	return &File{
+		FileId:   res.Result.FileId,
+		FilePath: res.Result.FilePath,
+	}, nil
+}
+
+func (c *Client) DownloadFile(filePath string) error {
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join("file/" + c.basePath + "/" + filePath),
+	}
+
+	_, err := grab.Get(config.VoicesFileDirectory, u.String())
+	if err != nil {
+		return helpers.ErrWrapIfNotNil("can't download file", err)
+	}
+
+	return nil
+}
+
 func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
 	defer func() { err = helpers.ErrWrapIfNotNil("can't send request", err) }()
 
 	u := url.URL{
 		Scheme: "https",
 		Host:   c.host,
-		Path:   path.Join(c.basePath + "method"),
+		Path:   path.Join(c.basePath + method),
 	}
 
 	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
@@ -97,4 +139,15 @@ func (c *Client) doRequest(method string, query url.Values) (data []byte, err er
 	}
 
 	return body, nil
+}
+
+func (c *Client) fileResponse(data []byte) (*GetFileResponse, error) {
+	var r GetFileResponse
+
+	err := json.Unmarshal(data, &r)
+	if err != nil {
+		return nil, helpers.ErrWrapIfNotNil("can't deserialize json response", err)
+	}
+
+	return &r, nil
 }
---
+++ internal/client/telegram/types.go
@@ -0,0 +1,41 @@
+package telegram
+
+type Update struct {
+	Id      int              `json:"update_id"`
+	Message *IncomingMessage `json:"message"`
+}
+
+type UpdatesResponse struct {
+	Ok     bool     `json:"ok"`
+	Result []Update `json:"result"`
+}
+
+type IncomingMessage struct {
+	Text  string `json:"text"`
+	From  User   `json:"from"`
+	Chat  Chat   `json:"chat"`
+	Voice Voice  `json:"voice"`
+}
+
+type Voice struct {
+	FileId string `json:"file_id"`
+}
+
+type User struct {
+	Username string `json:"username"`
+}
+
+type Chat struct {
+	ID int `json:"id"`
+}
+
+type File struct {
+	FileId   string `json:"file_id"`
+	FilePath string `json:"file_path,omitempty"`
+}
+
+type GetFileResponse struct {
+	Ok     bool   `json:"ok"`
+	Error  string `json:"error"`
+	Result File   `json:"result"`
+}
