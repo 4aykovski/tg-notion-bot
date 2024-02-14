076f9e6f48ac0e16463816a8f4cc2e7f50dfd575
4aykovski <kan89kan@gmail.com>
2024-02-14 13:01:57 +0700

    fix: remove doRequestPost method from gigaChatClient and replace it with httpClient methods

--- internal/client/gigachat/gigachat.go
+++ internal/client/gigachat/gigachat.go
@@ -3,13 +3,11 @@ package gigachat
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"net/url"
-	"path"
 	"strings"
 
 	"github.com/4aykovski/tg-notion-bot/config"
+	"github.com/4aykovski/tg-notion-bot/internal/client"
 )
 
 const (
@@ -18,10 +16,9 @@ const (
 )
 
 type Client struct {
-	host     string
-	basePath string
-	token    string
-	client   http.Client
+	hTTPClient client.HTTPClient
+	token      string
+	client     http.Client
 }
 
 func New(cfg config.GigaChatConfig) (*Client, error) {
@@ -29,66 +26,42 @@ func New(cfg config.GigaChatConfig) (*Client, error) {
 		return nil, fmt.Errorf("can't create gigachat client: %w", fmt.Errorf("token wasn't specified"))
 	}
 	return &Client{
-		host:     cfg.Host,
-		basePath: cfg.APIBasePath,
-		token:    cfg.Token,
-		client:   http.Client{},
+		hTTPClient: *client.NewHTTPClient(cfg.Host, cfg.APIBasePath),
+		token:      cfg.Token,
+		client:     http.Client{},
 	}, nil
 }
 
-func (c *Client) Completions(text string) (result string, err error) {
+func (c *Client) Completions(text string) (string, error) {
 
 	rB := newRequestBody(text)
 
-	body, err := json.Marshal(rB)
+	jsonRB, err := json.Marshal(rB)
 	if err != nil {
 		return "", fmt.Errorf("can't get completion: %w", err)
 	}
 
-	r, err := c.doRequest(completionsMethod, strings.NewReader(string(body)))
+	u := c.hTTPClient.GetUlrWithMethods(completionsMethod)
+	header := http.Header{}
+	header.Add("Authorization", "Bearer "+c.token)
+	header.Add("Content-Type", contentTypeJson)
+	header.Add("Accept", contentTypeJson)
+	body := strings.NewReader(string(jsonRB))
+
+	req, err := c.hTTPClient.CreateRequest(http.MethodPost, u.String(), header, body, nil)
 	if err != nil {
 		return "", fmt.Errorf("can't get completion: %w", err)
 	}
 
-	var respBody responseBody
-	if err := json.Unmarshal(r, &respBody); err != nil {
+	res, err := c.hTTPClient.Do(req)
+	if err != nil {
 		return "", fmt.Errorf("can't get completion: %w", err)
 	}
 
-	return respBody.Choices[0].Message.Content, nil
-}
-
-func (c *Client) doRequest(method string, requestBody io.Reader) (result []byte, err error) {
-
-	u := url.URL{
-		Scheme: "https",
-		Host:   c.host,
-		Path:   path.Join(c.basePath, method),
+	var result responseBody
+	if err := json.Unmarshal(res, &result); err != nil {
+		return "", fmt.Errorf("can't get completion: %w", err)
 	}
 
-	req, err := http.NewRequest(http.MethodPost, u.String(), requestBody)
-	if err != nil {
-		return nil, fmt.Errorf("can't do request to gigachat: %w", err)
-	}
-
-	req.Header.Add("Authorization", "Bearer "+c.token)
-	req.Header.Add("Content-Type", contentTypeJson)
-	req.Header.Add("Accept", contentTypeJson)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("can't do request to gigachat: %w", err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("can't do request to gigachat: %w", fmt.Errorf("wrong status code: %d", res.StatusCode))
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("can't do request to gigachat: %w", err)
-	}
-
-	return body, nil
+	return result.Choices[0].Message.Content, nil
 }
