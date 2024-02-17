970defc53be8ad884b873c1ec25080ec179a0154
4aykovski <128985570+4aykovski@users.noreply.github.com>
2024-02-17 13:21:32 +0300

    Feat: logic to retry request if get 401 status code  (#2)
    * feat: specified errors to track 401 status code
    
    * fix: remove unnecessary info from ERROR log
    
    * fix: add auth token to gigachat config
    
    * feat: add retry logic to gigaChat client
    
    * fix: generalize ErrAuthInfoNotSpecified for all clients
    
    * fix: generalize consts for header contentType
    
    * fix: generalize ErrCantUpdate token to all clients
    
    * fix: remove unnecessary function
    
    * fix: remove unnecessary function
    
    * feat: add retry logic to SaluteSpeech Client
    
    * fix: change env.template
    
    * fix: named errors

--- internal/client/gigachat/gigachat.go
+++ internal/client/gigachat/gigachat.go
@@ -2,8 +2,10 @@ package gigachat
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/4aykovski/tg-notion-bot/config"
@@ -11,24 +13,32 @@ import (
 )
 
 const (
-	contentTypeJson   = "application/json"
 	completionsMethod = "/chat/completions"
+	authUrl           = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+	dataScope         = "GIGACHAT_API_PERS"
+	rqUid             = "6f0b1291-c7f3-43c6-bb2e-9f3efb2dc98e"
+)
+
+var (
+	ErrCantGetCompletion         = errors.New("can't get completion")
+	ErrCantSendCompletionRequest = errors.New("can't send completion request")
 )
 
 type Client struct {
 	hTTPClient *client.HTTPClient
 	token      string
-	client     http.Client
+	auth       string
 }
 
 func New(cfg config.GigaChatConfig) (*Client, error) {
-	if cfg.Token == "" {
-		return nil, fmt.Errorf("can't create gigachat client: %w", fmt.Errorf("token wasn't specified"))
+	if cfg.Token == "" && cfg.Auth == "" {
+		return nil, fmt.Errorf("gigachat %w: %w", client.ErrCantCreateClient, client.ErrAuthInfoNotSpecified)
 	}
+
 	return &Client{
 		hTTPClient: client.NewHTTPClient(cfg.Host, cfg.APIBasePath),
 		token:      cfg.Token,
-		client:     http.Client{},
+		auth:       cfg.Auth,
 	}, nil
 }
 
@@ -38,30 +48,80 @@ func (c *Client) Completions(text string) (string, error) {
 
 	jsonRB, err := json.Marshal(rB)
 	if err != nil {
-		return "", fmt.Errorf("can't get completion: %w", err)
+		return "", fmt.Errorf("%w: %w", ErrCantGetCompletion, err)
 	}
 
-	u := c.hTTPClient.GetUlrWithMethods(completionsMethod)
-	header := http.Header{}
-	header.Add("Authorization", "Bearer "+c.token)
-	header.Add("Content-Type", contentTypeJson)
-	header.Add("Accept", contentTypeJson)
-	body := strings.NewReader(string(jsonRB))
+	res, err := c.completionRequest(jsonRB)
+	if err != nil && !errors.Is(err, client.Err401StatusCode) {
+		return "", fmt.Errorf("%w: %w", ErrCantGetCompletion, err)
+	} else if errors.Is(err, client.Err401StatusCode) {
+		if err = c.updateToken(); err != nil {
+			return "", fmt.Errorf("%w: %w", ErrCantGetCompletion, err)
+		}
 
-	req, err := c.hTTPClient.CreateRequest(http.MethodPost, u.String(), header, body, nil)
-	if err != nil {
-		return "", fmt.Errorf("can't get completion: %w", err)
-	}
-
-	res, err := c.hTTPClient.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("can't get completion: %w", err)
+		res, err = c.completionRequest(jsonRB)
+		if err != nil {
+			return "", fmt.Errorf("%w: %w", ErrCantGetCompletion, err)
+		}
 	}
 
 	var result responseBody
 	if err := json.Unmarshal(res, &result); err != nil {
-		return "", fmt.Errorf("can't get completion: %w", err)
+		return "", fmt.Errorf("%w: %w", ErrCantGetCompletion, err)
 	}
 
 	return result.Choices[0].Message.Content, nil
+}
+
+func (c *Client) completionRequest(jsonBody []byte) ([]byte, error) {
+	u := c.hTTPClient.GetUlrWithMethods(completionsMethod)
+	header := http.Header{
+		"Authorization": []string{"Bearer " + c.token},
+		"Content-Type":  []string{client.ContentTypeJson},
+		"Accept":        []string{client.ContentTypeJson},
+	}
+
+	req, err := c.hTTPClient.CreateRequest(http.MethodPost, u.String(), header, strings.NewReader(string(jsonBody)), nil)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", ErrCantSendCompletionRequest, err)
+	}
+
+	res, err := c.hTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", ErrCantSendCompletionRequest, err)
+	}
+
+	return res, nil
+}
+
+func (c *Client) updateToken() error {
+	u := authUrl
+
+	header := http.Header{}
+	header.Add("Authorization", "Bearer "+c.auth)
+	header.Add("Content-Type", client.ContentTypeUrlEncoded)
+	header.Add("RqUID", rqUid)
+	header.Add("Accept", client.ContentTypeJson)
+
+	body := url.Values{}
+	body.Set("scope", dataScope)
+
+	req, err := c.hTTPClient.CreateRequest(http.MethodPost, u, header, strings.NewReader(body.Encode()), nil)
+	if err != nil {
+		return fmt.Errorf("%w: %w", client.ErrCantUpdateToken, err)
+	}
+
+	res, err := c.hTTPClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("%w: %w", client.ErrCantUpdateToken, err)
+	}
+
+	var result responseBody
+	err = json.Unmarshal(res, &result)
+	if err != nil {
+		return fmt.Errorf("%w: %w", client.ErrCantUpdateToken, err)
+	}
+
+	c.token = result.AccessToken
+	return nil
 }
--- internal/client/salutespeech/types.go
+++ internal/client/salutespeech/types.go
@@ -1,7 +1,8 @@
 package salutespeech
 
 type Response struct {
-	StatusCode int      `json:"status,omitempty"`
-	Message    string   `json:"message,omitempty"`
-	Result     []string `json:"result,omitempty"`
+	StatusCode  int      `json:"status,omitempty"`
+	Message     string   `json:"message,omitempty"`
+	Result      []string `json:"result,omitempty"`
+	AccessToken string   `json:"access_token,omitempty"`
 }
